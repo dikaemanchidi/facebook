de130c085f068ee29b6d778b5c2f5dda401129f6
dikaemanchidi <dikaemanchidi@gmail.com>
2021-06-21 23:49:48 +0900

    facebookclone

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,10 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
-
-  # GET /posts or /posts.json
   def index
     @posts = Post.all
   end
-
-  # GET /posts/1 or /posts/1.json
   def show
   end
-
-  # GET /posts/new
     def new
       if params[:back]
         @post = Post.new(post_params)
@@ -18,14 +12,10 @@ class PostsController < ApplicationController
     @post = current_user.posts.build
   end
 end
-  # GET /posts/1/edit
   def edit
   end
-
-  # POST /posts or /posts.json
   def create
     @post = current_user.posts.build(post_params)
-
     respond_to do |format|
       if @post.save
         format.html { redirect_to @post, notice: "Post was successfully created." }
@@ -36,8 +26,6 @@ end
       end
     end
   end
-
-  # PATCH/PUT /posts/1 or /posts/1.json
   def update
     respond_to do |format|
       if @post.update(post_params)
@@ -53,8 +41,6 @@ end
         @post = Post.new(post_params)
         render :new if @post.invalid?
       end
-
-  # DELETE /posts/1 or /posts/1.json
   def destroy
     @post.destroy
     respond_to do |format|
@@ -62,15 +48,11 @@ end
       format.json { head :no_content }
     end
   end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def post_params
-      params.require(:post).permit(:image, :image_cache, :content, :user_id)
-    end
+  def set_post
+    @post = Post.find(params[:id])
+  end
+  def post_params
+    params.require(:post).permit(:image, :image_cache, :content, :user_id)
+  end
 end
